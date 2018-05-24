729846c0c36abd176314fa5e32951dde13e4eee2
Nick <nicholasha01@gmail.com>
2018-05-24 16:07:19 -0700

    vect_stats is normal stats

--- filter_code/nick/vect_stats.m
+++ filter_code/nick/vect_stats.m
@@ -1,27 +1,7 @@
-% function [ mu,covar,diffs ] = vect_stats( X,alpha_mu,alpha_cov)
-% % X column of arbitrary data
-% [n,n_vects] = size(X);
-% mu = alpha_mu*X(:,1) + sum(X(:,2:end),2)/(2*n_vects);
-% 
-% diffs = zeros(n,n_vects);
-% covar = zeros(n);
-% for i=1:n_vects
-%     diffs(:,i) = X(:,i) - mu;
-%     A = diffs(:,i); %change from diffs to diffs(:,i)
-%     covar = covar + A*A';
-% end 
-% covar = covar/(2*n_vects);
-% 
-% A = (X(:,1) - mu);
-% covar = covar + alpha_cov*(A*A');
-% 
-% end
-
-%version 2 --wq
 function [ mu,covar,diffs ] = vect_stats( X,alpha_mu,alpha_cov)
 % X column of arbitrary data
 [n,n_vects] = size(X);
-mu =  sum(X(:,2:end),2)/(n_vects-1);
+mu = (alpha_mu*X(:,1) + sum(X(:,2:end),2))/(n_vects);
 
 diffs = zeros(n,n_vects);
 covar = zeros(n);
@@ -30,9 +10,28 @@ for i=2:n_vects
     A = diffs(:,i); %change from diffs to diffs(:,i)
     covar = covar + A*A';
 end 
-covar = covar/(n_vects-1);
 
-% A = (X(:,1) - mu);
-% covar = covar + alpha_cov*(A*A');
+A = (X(:,1) - mu);
+covar = (covar + alpha_cov*(A*A'))/n_vects;
 
 end
+
+% %version 2 --wq
+% function [ mu,covar,diffs ] = vect_stats( X,alpha_mu,alpha_cov)
+% % X column of arbitrary data
+% [n,n_vects] = size(X);
+% mu =  sum(X(:,2:end),2)/(n_vects-1);
+% 
+% diffs = zeros(n,n_vects);
+% covar = zeros(n);
+% for i=2:n_vects
+%     diffs(:,i) = X(:,i) - mu;
+%     A = diffs(:,i); %change from diffs to diffs(:,i)
+%     covar = covar + A*A';
+% end 
+% covar = covar/(n_vects-1);
+% 
+% % A = (X(:,1) - mu);
+% % covar = covar + alpha_cov*(A*A');
+
+%end
