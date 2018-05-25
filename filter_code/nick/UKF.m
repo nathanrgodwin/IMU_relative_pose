abab8a49e069c2c645df7b7a7855171f796c1d0c
Nick <nicholasha01@gmail.com>
2018-05-24 17:12:32 -0700

    changed storage of values

--- filter_code/nick/UKF.m
+++ filter_code/nick/UKF.m
@@ -21,28 +21,28 @@ n_steps= size(data,2);
 % g_quat = [0;0;0;1];%gravity vector, units of g 
 
 %estimates
-x_hat = zeros(n_state,n_steps);
-x_hat(:,1) = [1 0 0 0,1 0 0 0,0 0 0,0 0 0, 0 0 0, 0 0 0]';
-P_hat = zeros(n_state_vect,n_state_vect,n_steps);
-P_hat(:,:,1) = .001*eye(n_state_vect);
+x_hatM = zeros(n_state,n_steps);
+x_hatM(:,1) = [1 0 0 0,1 0 0 0,0 0 0,0 0 0, 0 0 0, 0 0 0]';
+P_hatM = zeros(n_state_vect,n_state_vect,n_steps);
+P_hatM(:,:,1) = .001*eye(n_state_vect);
 
 % x t|t-1 . ap = 'a priori', after prediction step
-x_ap = zeros(n_state,n_steps);
-P_ap = zeros(n_state_vect,n_state_vect,n_steps);
+x_apM = zeros(n_state,n_steps);
+P_apM = zeros(n_state_vect,n_state_vect,n_steps);
 
 %measurements z: 12, 18 including orientations from matlab
 z = data;
 
 % expected measurement z at t 
-z_ap = zeros(n_meas,n_steps); 
-P_zz = zeros(n_meas,n_meas,n_steps);
+z_apM = zeros(n_meas,n_steps); 
+P_zzM = zeros(n_meas,n_meas,n_steps);
 
 %innovation nu = z - z_ap
-nu = zeros(n_meas,n_steps);
-P_nu = zeros(n_meas,n_meas,n_steps);
+nuM = zeros(n_meas,n_steps);
+P_nuM = zeros(n_meas,n_meas,n_steps);
 
-P_xz = zeros(n_meas,n_meas,n_steps); % cross corr 
-K = zeros(n_meas,n_meas,n_steps); % kalman gain
+P_xzM = zeros(n_meas,n_meas,n_steps); % cross corr 
+KM = zeros(n_meas,n_meas,n_steps); % kalman gain
 
 X = zeros(n_state,n_sigma_points);%sigma points for x_hat
 Y = zeros(n_state_vect,n_sigma_points);%sigma points for x_ap
@@ -62,31 +62,44 @@ alpha_cov = .5; % or 2
 
 for i = 1: n_steps
     %PREDICTION
-    X = gen_sigma_points(x_hat(:,i),P_hat(:,:,i) + Q); 
+    X = gen_sigma_points(x_hatM(:,i),P_hatM(:,:,i) + Q); 
     Y = process_a(X);
-    [x_ap(:,i+1), P_ap(:,:,i+1), W_prime] = Y_stats(Y,alpha_mu,alpha_cov,x_hat(:,i));% stats from Y. W_prime: Y with x_ap subtracted from each. W_prime in vector space
+    [x_ap, P_ap, W_prime] = Y_stats(Y,alpha_mu,alpha_cov,x_hatM(:,i));% stats from Y. W_prime: Y with x_ap subtracted from each. W_prime in vector space
         %   ^ Winson changed i to i+1
     
     %UPDATE
     Z = measurement_h(Y);% measurement model h to get sigma points from Y
-    [z_ap(:,i+1), P_zz(:,:,i+1)] = Z_stats(Z,alpha_mu,alpha_cov, z_ap(:,i));
+    [z_ap, P_zz] = Z_stats(Z,alpha_mu,alpha_cov, z_apM(:,i));
     
-    nu(:,i+1) = z(:,i+1) - z_ap(:,i+1);
-    P_nu(:,:,i+1) = P_zz(:,:,i+1) + R;
-    P_xz(:,:,i+1) = cross_stats(W_prime,Z,alpha_mu,alpha_cov);
-    K(:,:,i+1) = P_xz(:,:,i+1)*P_nu(:,:,i+1)^-1;
+    nu = z(:,i+1) - z_ap;
+    P_nu = P_zz + R;
+    P_xz = cross_stats(W_prime,Z,alpha_mu,alpha_cov);
+    K = P_xz*P_nu^-1;
     % x_k|k = x_ap + K*nu
-    Kv = K(:,:,i+1)*nu(:,i+1);
+    Kv = K*nu;
     
-    x_hat(1:4,i+1) = quatproduct(x_ap(1:4,i+1), aa2quat(Kv(1:3)));
-    x_hat(5:8,i+1) = quatproduct(x_ap(5:8,i+1), aa2quat(Kv(4:6)));
+    x_hat = zeros(n_state,1);
+    x_hat(1:4) = quatproduct(x_ap(1:4), aa2quat(Kv(1:3)));
+    x_hat(5:8) = quatproduct(x_ap(5:8), aa2quat(Kv(4:6)));
 
-    x_hat(9:end,i+1) = x_ap(9:end,i+1) + Kv(7:end);
+    x_hat(9:end) = x_ap(9:end) + Kv(7:end);
 %     x_hat(1:4,i+1) = quatproduct( x_ap(1:4,i+1), aa2quat(K(1:4,1:4,i+1)*nu(1:3,i+1)) );
 %     x_hat(5:8,i+1) = quatproduct( x_ap(5:8,i+1), aa2quat(K(5:8,5:8,i+1)*nu(5:8,i+1)) );
 %     x_hat(9:end,i+1) = x_ap(9:end) + K(9:end,9:end,i+1)*nu(9:end,i+1);
     %P = P_ap - K*P_nu*K'
-    P_hat(:,:,i+1) = P_ap(:,:,i+1) - K(:,:,i+1)*P_nu(:,:,i+1)*K(:,:,i+1)';    
+    P_hat = P_ap - K*P_nu*K';
+    
+    %STORE MEMORY
+    x_hatM(:,i+1) = x_hat;
+    P_hatM(:,:,i+1) = P_hat;
+    x_apM(:,i+1) = x_ap;
+    P_apM(:,:,i+1) = P_ap;
+    z_apM(:,i+1) = z_ap;
+    P_zzM(:,:,i+1) = P_zz;
+    nuM(:,i+1) = nu;
+    P_nuM(:,:,i+1) = P_nu;
+    P_xzM(:,:,i+1) = P_xz;
+    KM(:,:,i+1) = K;
 end 
 
 %% plot results 
