abef980e86db9b13e8dc17b0405e763350c4b05c
Winson <rocketwings@gmail.com>
2018-05-24 20:50:39 -0700

    fixed zstats

--- filter_code/nick/Z_stats.m
+++ filter_code/nick/Z_stats.m
@@ -6,8 +6,8 @@ Z_q1 = aa2quat(Z(13:15,:));
 Z_q2 = aa2quat(Z(16:18,:));
 
 [z_mean_vect,covar_vect] = vect_stats(Z_vect,alpha_mu,alpha_cov);
-[z_q1,covar_q1, ~] = quat_stats(Z_q1,alpha_mu,alpha_cov,z0);
-[z_q2,covar_q2, ~] = quat_stats(Z_q2,alpha_mu,alpha_cov,z0);
+[z_q1,covar_q1, ~] = quat_stats(Z_q1,alpha_mu,alpha_cov,aa2quat(z0(15:17)));
+[z_q2,covar_q2, ~] = quat_stats(Z_q2,alpha_mu,alpha_cov,aa2quat(z0(16:18)));
 
 z_mean = [z_mean_vect;quat2aa(z_q1);quat2aa(z_q2)];
 covar = blkdiag(covar_vect,covar_q1,covar_q2);
