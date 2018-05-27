aa6ec08eb4ce7715b81e32d8737ad21d35e2f71d
Winson <rocketwings@gmail.com>
2018-05-27 16:36:56 -0700

    EKF works

--- filter_code/nick/EKF.m
+++ filter_code/nick/EKF.m
@@ -1,26 +1,57 @@
-
-ident_quat = [1 0 0 0]'
-om = omega([0.1 0 0]')
-
-result = 1/2 * om * ident_quat
+read_baseline_data;
 
 
+init_state = [1 0 0 0]';
+init_Sigma = eye(4)';
 
+gyro = gyro(:,:)/100;
+z = accel(:,:);
+[~, len] = size(z);
 
+x_hatM = zeros(4,len+1);
+x_hatM(:,1) = init_state;
 
+SigmaM = zeros(4,4,len+1);
+SigmaM(:,:,1) = init_Sigma;
 
+x_apM = zeros(4,len+1);
 
+W = eye(3) * 0.001;
+V = eye(3) * 0.01;
 
-function [om] = omega(w)
-    om = zeros(3);
-    wx = w(1);
-    wy = w(2);
-    wz = w(3);
+for i = 1:len
+   if i == 1
+       x_t = init_state;
+       Sigma_tt = init_Sigma;
+   else
+       x_t = x_hatM(:,i);
+       Sigma_tt = SigmaM(:,:,i);
+   end
+    
+   A = EKF_A(x_t);
+   Q = EKF_Q(x_t, gyro(:,i));
+   
+   x_ap = EKF_a(x_t, gyro(:,i));
+   Sigma_ap = A * Sigma_tt * A' + Q*W*Q';
 
-    om = ...
-    [   0   -wx -wy -wz ;
-        wx  0   wz  -wy ;
-        wy  -wz 0   wx  ;
-        wz  wy  -wx 0   ];
+   H = EKF_H(x_ap);
+   R = EKF_R;
+   K = (Sigma_ap * H') / (H*Sigma_ap*H' + R*V*R');
+   z = EKF_h(x_t);
 
+   x_hat = x_ap + K * (z - EKF_h(x_ap));
+   Sigma_hat = (eye(4) - K*H)*Sigma_ap;
+   
+   x_hatM(:,i+1) = x_hat;
+   SigmaM(:,:,i+1) = Sigma_hat;
 end
+
+
+readable_states = quatern2euler(x_hatM');
+figure();
+subplot(3,1,1)
+plot(readable_states(:,1))
+subplot(3,1,2)
+plot(readable_states(:,2))
+subplot(3,1,3)
+plot(readable_states(:,3))
