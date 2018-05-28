7e3ddd945068e6b5a927b33c90d240df6d961446
Nick <nicholasha01@gmail.com>
2018-05-27 17:18:32 -0700

    v2

---
+++ filter_code/nick/baselineUKF_v2/run_filters.m
@@ -0,0 +1,10 @@
+clear all
+close all
+
+%reads data from 276a dataset as ax ay az wx wy wz as a wide matrix
+data = csvread('data_imu_276a_dset1.csv');
+t = csvread('t_imu_276a_dataset1.csv');
+data(1:3,:) = data(1:3,:); %
+
+[xhat_1,f_1] = baselineUKF(data,t);
+
