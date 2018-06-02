7d18d35563cfc3c4541560439956fee187dddb92
Winson <rocketwings@gmail.com>
2018-06-02 16:10:32 -0700

    idk what changd

--- filter_code/nick/baselineUKF_v2/EKF_A.m
+++ filter_code/nick/baselineUKF_v2/EKF_A.m
@@ -1,11 +1,13 @@
-function [ A ] = EKF_A( state )
+function [ A ] = EKF_A( omega )
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 
-q1 = state(1);
-q2 = state(2);
-q3  = state(3);
-q4 = state(4);
+q = aa2quat(omega);
+
+q1 = q(1);
+q2 = q(2);
+q3  = q(3);
+q4 = q(4);
 
 A = ...
     [   q1  -q2 -q3 -q4;
