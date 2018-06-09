df640bd07382bdef04eaea699d663fc4ddf67817
Nick <nicholasha01@gmail.com>
2018-06-09 14:28:39 -0700

    working integration

--- filter_code/nick/baselineUKF_v2/run_filter_Tstick.m
+++ filter_code/nick/baselineUKF_v2/run_filter_Tstick.m
@@ -7,22 +7,23 @@
 %[t,vicon,data,data_centered_1,data_centered_2,b_vect,mean_a,mean_w,mean_b] = inspect_stationary();
 
 %% 
-[x_hat,fig] = UKF_4(data,t);
-figure(fig)
-title('UKF4');
-[x_hat_mag,fig] = UKF_4_mag(data,t,b_vect,fig);
+fig = figure;
+[x_hat_i]=simple_integration(data,t,fig);
+[x_hat] = UKF_4(data,t,fig);
+[x_hat_mag] = UKF_4_mag(data,t,b_vect,fig);
 title('raw')
 
-[x_hat_1,fig] = UKF_4(data_centered_1,t);
-figure(fig)
-title('UKF4');
-[x_hat_mag_1,fig] = UKF_4_mag(data_centered_1,t,b_vect,fig);
-title('centered gyro')
+fig_1 = figure;
+[x_hat_i_1]=simple_integration(data_centered_1,t,fig_1);
+[x_hat_1] = UKF_4(data_centered_1,t,fig_1);
+[x_hat_mag_1] = UKF_4_mag(data_centered_1,t,b_vect,fig_1);
+title('centered gyro');
 
-[x_hat_2,fig] = UKF_4(data_centered_2,t);
-figure(fig)
-title('UKF4 no mag');
-[x_hat_mag_1,fig] = UKF_4_mag(data_centered_2,t,b_vect,fig);
+fig_2 = figure;
+[x_hat_i_2]=simple_integration(data_centered_2,t,fig_2);
+[x_hat_2] = UKF_4(data_centered_2,t,fig_2);
+[x_hat_mag_2] = UKF_4_mag(data_centered_2,t,b_vect,fig_2);
+
 title('centered gyro and accel')
 
 %% non-stationary
