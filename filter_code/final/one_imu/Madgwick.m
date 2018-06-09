39b60ca6aa7af62a36d652d4d4cbdb5df073e729
Nathan Godwin <nathanrgodwin@gmail.com>
2018-06-09 16:07:05 -0700

    Changed Madgwick beta value

--- filter_code/final/one_imu/Madgwick.m
+++ filter_code/final/one_imu/Madgwick.m
@@ -1,11 +1,11 @@
 function [dout,fig]=Madgwick(din,t)
     addpath('quaternion_library');
     dt = mean(t(2:end) - t(1:end-1));
-    AHRS = MadgwickAHRS('SamplePeriod', dt, 'Beta', 0.1);
+    AHRS = MadgwickAHRS('SamplePeriod', dt, 'Beta', 0.003);
 
     quaternion = zeros(length(t), 4);
     for i = 1:length(t)
-        AHRS.Update(din(i,4:6), din(i,1:3), din(i,7:9)/1000);
+        AHRS.Update(din(i,4:6), din(i,1:3), din(i,7:9));
         quaternion(i, :) = AHRS.Quaternion;
     end
     dout = quaternion';
