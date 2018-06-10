5d9e6bfc6b0125ce2f74bab3a95111dba3bdf1a2
Nick <nicholasha01@gmail.com>
2018-06-09 17:35:55 -0700

    different markers and linestyle

--- filter_code/final/one_imu/result_accuracy.m
+++ filter_code/final/one_imu/result_accuracy.m
@@ -1,12 +1,14 @@
 fc = 0;
 
+style = {'--',':','-.','-'};
+style = [style style style style]
 for data_idx = data_idxs
     fc=fc+1;    figure();
     temp_state = truths{data_idx};
     temp_state = quat2aa(temp_state(1:4,:));
     for idx = 1:3             
         subplot(3,1,idx);  hold on;
-        plot(times{data_idx},temp_state(idx,:),'k');
+        plot(times{data_idx},temp_state(idx,:),'k','LineWidth',1.5);
         if idx==1
             title(sprintf('set %g: %s',data_idx,names{data_idx}), 'Interpreter', 'none');
 %             fprintf('sdfsdf\n');
@@ -17,7 +19,7 @@ for data_idx = data_idxs
         temp_state = quat2aa(temp_state(1:4,:));
         for idx = 1:3
             subplot(3,1,idx);  hold on;
-            plot(times{data_idx},temp_state(idx,:));
+            plot(times{data_idx},temp_state(idx,:),style{meth_idx},'LineWidth',1.1);
         end
     end   
     temp_names = {'truth'};
