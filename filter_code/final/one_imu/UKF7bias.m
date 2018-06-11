519f5f6e7a49a158c902a158dd15d607c3034cc9
Winson <rocketwings@gmail.com>
2018-06-10 17:30:23 -0700

    bias versions of UKF

---
+++ filter_code/final/one_imu/UKF7bias.m
@@ -0,0 +1,153 @@
+function [x_hatM, P_hatM] = UKF7bias(data,t,Q,R)
+
+if size(data,1) > 8
+    data = data(1:6,:);
+end
+
+%measurements AxAyAz from I IMU
+z = data(1:6,:); %measurements
+% u = data(4:6,:);%data from 276a
+
+%% states
+% 1: G_R_I = 4x1 quat
+
+n_state = 10;
+n_state_vect = n_state - 1; %since 1 quat --> vect
+n_sigma_points = n_state_vect*2+1;
+n_meas = 6;
+n_steps= size(data,2);
+
+g_quat = [0;0;0;9.8];%gravity vector, units of m/s^2
+
+%M indicates storage variable
+%hat indicates estimate
+x_hatM = zeros(n_state,n_steps);
+x_hatM(1:4,1) = [1 0 0 0];
+P_hatM = zeros(n_state_vect,n_state_vect,n_steps);
+P_hatM(:,:,1) = .0001*eye(n_state_vect);
+
+% x t|t-1 . ap = 'a priori', after prediction step
+x_apM = zeros(n_state,n_steps);
+P_apM = zeros(n_state_vect,n_state_vect,n_steps);
+
+% expected measurement z at t 
+z_apM = zeros(n_meas,n_steps); 
+P_zzM = zeros(n_meas,n_meas,n_steps);
+
+%innovation nu = z - z_ap
+nuM = zeros(n_meas,n_steps);
+P_nuM = zeros(n_meas,n_meas,n_steps);
+
+P_xzM = zeros(n_state_vect,n_meas,n_steps); % cross corr 
+KM = zeros(n_state_vect,n_meas,n_steps); % kalman gain
+
+%X = zeros(n_state,n_sigma_points);%sigma points for x_hat
+Y = zeros(n_state,n_sigma_points);%sigma points for x_ap
+Z = zeros(n_meas,n_sigma_points);%sigma points for z_ap
+
+%noise covariances. assumed diagonal
+%orientation, process noise will be in rot vel perturbations converted to quats
+
+if nargin < 4
+    %noise covariances. assumed diagonal
+    %orientation, process noise will be in rot vel perturbations converted to quats
+    q = .001;
+    r = .01*9.8^2;
+    Q = q*eye(n_state_vect);
+    R = r*eye(n_meas);
+    
+    Q = blkdiag(diag([1 1 0.1]*1e-4), eye(3)*1e-5, eye(3));
+    
+    R = blkdiag(eye(3)*1e2, eye(3)*1e2);
+    
+end
+
+%weights for averaging 
+alpha_mu = 0; %or 0
+alpha_cov = 2; % or 2 
+
+for itr = 1: n_steps-1
+    dt = t(itr+1) - t(itr);
+    %PREDICTION
+    X = gen_sigma_points7(x_hatM(:,itr),P_hatM(:,:,itr) + Q); 
+%     figure(1)
+%     surf(X); xlabel('x');ylabel('y');title('X');
+    for i_sp=1:n_sigma_points
+        Y(1:4,i_sp) = quatproduct(X(:,i_sp), aa2quat(X(5:7,i_sp))*dt);
+        Y(5:10,i_sp) = X(5:10,i_sp);
+    end 
+%     figure(2)
+%     surf(Y); xlabel('x');ylabel('y');title('Y');
+    [x_ap, P_ap, W_prime] = Y_stats7(Y,alpha_mu,alpha_cov,x_hatM(:,itr));% stats from Y. W_prime: Y with x_ap subtracted from each. W_prime in vector space
+    
+    %UPDATE  
+    for i_sp = 1:n_sigma_points
+        tempg = quatproduct(g_quat,Y(1:4,i_sp));
+        temp_accel = quatproduct([Y(1,i_sp);-Y(2:4,i_sp)],tempg);
+
+        bias = Y(8:10,i_sp);
+        temp_gyro = Y(4:6,i_sp) - bias;
+
+        Z(:,i_sp) = [temp_accel(2:4); temp_gyro];
+    end 
+    
+    [z_ap, P_zz] = Z_stats7(Z,alpha_mu,alpha_cov);
+    
+    nu = z(:,itr+1) - z_ap;
+    P_nu = P_zz + R;
+    P_xz = cross_stats(W_prime,Z,alpha_mu, alpha_cov);
+    K = P_xz*P_nu^-1;
+    
+    Kv = K*nu;
+    % x_k|k = x_ap + K*nu
+    
+    x_hat = [quatproduct(x_ap(1:4), aa2quat(Kv(1:3))); x_ap(5:end)+Kv(4:end)];
+    P_hat = P_ap - K*P_nu'*K';
+    
+    if min(eig(P_hat)) < 0
+        P_hat = nearestSPD(P_hat);
+        fprintf('non PSD P_hat\n');
+    end 
+    %STORE MEMORY
+    x_hatM(:,itr+1) = x_hat;
+    P_hatM(:,:,itr+1) = P_hat;
+    x_apM(:,itr+1) = x_ap;
+    P_apM(:,:,itr+1) = P_ap;
+    z_apM(:,itr+1) = z_ap;
+    P_zzM(:,:,itr+1) = P_zz;
+    nuM(:,itr+1) = nu;
+    P_nuM(:,:,itr+1) = P_nu;
+    P_xzM(:,:,itr+1) = P_xz;
+    KM(:,:,itr+1) = K;
+end 
+
+
+%% put x_hat to csv for python to read
+%csvwrite('x_hat_from_UKFmatlab.csv',x_hatM);
+
+%% plot results 
+
+%to use python results:
+%x_hatM = csvread('x_hat_from_UKFpython.csv');
+
+% figure
+% surf(x_hatM,'edgecolor', 'None');
+% plot(t,x_hatM);
+
+% eulers = 180/pi*quatern2euler(x_hatM');
+% 
+% fig = figure;
+% subplot(3,1,1)
+% plot(t,eulers(:,1));
+% subplot(3,1,2)
+% plot(t,eulers(:,2));
+% subplot(3,1,3)
+% plot(t,eulers(:,3));
+
+% x_hat_python = csvread('x_hat_from_UKFpython.csv');
+% x_hat_err = x_hat_python - x_hatM;
+% figure; surf(x_hat_err)
+
+end 
+
+
